5e361cbcf9fe0edb11500931f4748d458d53803f
Vic <19528516+vicgolding@users.noreply.github.com>
2026-06-05 19:15:54 +0200

    FIX: Correct and refactor AbsenceRequest domain model
    - Correct data types
    - Adjust access modifiers
    - Complete constructor
    - Add domain methods

--- Absence-Management-API/Domain/Entities/AbsenceRequest.cs
+++ Absence-Management-API/Domain/Entities/AbsenceRequest.cs
@@ -1,14 +1,55 @@
+using Absence_Management_API.Domain.Enums;
+
 namespace Absence_Management_API.Domain.Entities;
 
 public class AbsenceRequest
 {
-    public Guid Id { get; set; }
-    public int EmployeeId { get; set; }
-    public string AbsenceType { get; set; }
-    public string AbsenceStatus { get; set; }
-    public struct StartDate;
-    public struct EndDate;
-    public string Comment { get; set; }
+    public Guid Id { get; private set; }
+    public int EmployeeId { get; private set; }
+    public AbsenceType AbsenceType { get; private set; }
+    public AbsenceStatus AbsenceStatus { get; private set; } 
+    private DateTime _startDate;
+    private DateTime _endDate;
+    public string Comment { get; private set; }
+
+    public AbsenceRequest(Guid id, int employeeId, AbsenceType absenceType, AbsenceStatus absenceStatus,
+        DateTime startDate, DateTime endDate, string? comment = null)
+    {
+        Id = Guid.NewGuid();
+        EmployeeId = employeeId;
+        AbsenceType = absenceType;
+        AbsenceStatus = 0;
+        _startDate = startDate;
+        _endDate = endDate;
+    }
     
-    public AbsenceRequest() {}
+    public void ApproveRequest()
+    {
+        try
+        {
+            if ( _startDate >= _endDate || _endDate <= _startDate || AbsenceStatus != 0 )
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An exception has occurred!", e.Message);
+        }
+    }
+
+    public void DeclineRequest()
+    {
+        try
+        {
+            if (_startDate >= _endDate || _endDate <= _startDate || AbsenceStatus != 0)
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An exception has occurred!", e.Message);   
+        }
+    }
 }
