48db503eb4e4dac02e03cec9818e8de7a2fbecc9
Vic Golding <19528516+vicgolding@users.noreply.github.com>
2026-07-01 08:26:10 +0200

    Prepare update and delete request

--- Absence-Management-API/client-app/src/App.js
+++ Absence-Management-API/client-app/src/App.js
@@ -5,9 +5,11 @@ import { Col, Table, Button, ButtonGroup, Form, FormGroup, Label, Input } from '
 
 function App() {
   const [ absences, setAbsences ] = useState([]);
-
+  
+  const path = "https://localhost:5013/api/absence-requests";
+  
   useEffect(() => {
-    fetch("https://localhost:5013/api/absence-requests")
+    fetch(path)
         .then(response => response.json())
         .then((data) => setAbsences(data))
         .catch(err => console.error("API Error:", err));
@@ -28,6 +30,28 @@ function App() {
     ]);
   }
   
+  async function updateRequest(event) {
+    const requestId = event.target.dataset.id;
+    try {
+      const response = await fetch(`${path}/${requestId}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  async function deleteRequest(event) {
+    const requestId = event.target.dataset.id;
+    try {
+      const response = await fetch(`${path}/${requestId}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   async function processFormData(formData) {
     const employeeName = formData.get("employeeName");
     const absenceType = parseFloat(formData.get("selectAbsenceType"));
@@ -46,7 +70,7 @@ function App() {
     );
     try {
       const response = await fetch(
-          "https://localhost:5013/api/absence-requests", {
+          path, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: data,
@@ -127,33 +151,17 @@ function App() {
                   </td>
                   <td>
                     <ButtonGroup>
-                      <Button color="warning pt-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                             fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                          <path
-                              d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                          <path fill-rule="evenodd"
-                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-                        </svg>
+                      <Button 
+                          data-id={absence.id} 
+                          color="warning" 
+                          onClick={updateRequest}>
+                        Update
                       </Button>
-                      <Button color="danger pt-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                             fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
-                          <path
-                              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                          <path
-                              d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                        </svg>
-                      </Button>
-                      <Button color="success pt-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                             fill="currentColor" className="bi bi-check2-square" viewBox="0 0 16 16">
-                          <path
-                              d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/>
-                          <path
-                              d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/>
-                        </svg>
-                      </Button>
+                      <Button
+                          data-id={absence.id}
+                          color="danger"
+                          onClick={deleteRequest}>
+                        Delete</Button>
                     </ButtonGroup>
                   </td>
                 </tr>
