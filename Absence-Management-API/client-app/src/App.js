0e24efde036b1c0b0ea4ebaa328796458e445246
Vic Golding <19528516+vicgolding@users.noreply.github.com>
2026-07-01 15:31:14 +0200

    Refactor

--- Absence-Management-API/client-app/src/App.js
+++ Absence-Management-API/client-app/src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
+import RequestTableRows from './components/RequestTableRows';
+import RequestForm from './components/RequestForm';
 import './App.css';
 import './bootstrap.min.css';
-import { Col, Table, Button, ButtonGroup, Form, FormGroup, Label, Input } from 'reactstrap';
+import Table from 'reactstrap';
 
 function App() {
   const [ absences, setAbsences ] = useState([]);
@@ -156,42 +158,6 @@ function App() {
     }
   }
   
-  const currentRequests = absences.map((absence) => (
-        <tr>
-          <th scope="row">{absence.id}</th>
-          <td>{absence.employeeName}</td>
-          <td>{absence.absenceType}</td>
-          <td>{absence.startDate.slice(0, 10)}</td>
-          <td>{absence.endDate.slice(0, 10)}</td>
-          <td>{absence.comment}</td>
-          <td>{absence.absenceStatus}</td>
-          <td>
-            <ButtonGroup>
-              <Button
-                data-id={absence.id}
-                color="success"
-                onClick={approveRequest}
-              >
-                Approve
-              </Button>
-              <Button
-                  data-id={absence.id}
-                  color="warning"
-                  onClick={denyRequest}
-              >
-                Deny
-              </Button>
-              <Button
-                  data-id={absence.id}
-                  color="danger"
-                  onClick={deleteRequest}>
-                Delete
-              </Button>
-            </ButtonGroup>
-          </td>
-        </tr>
-  ));
-  
   return (
     <div className="App">
       <div className="container-fluid px-5 my-5">
@@ -232,7 +198,7 @@ function App() {
             </tr>
             </thead>
             <tbody>
-            {currentRequests}
+            <RequestTableRows requests={absences} />
             </tbody>
           </Table>
       </div>
@@ -240,97 +206,7 @@ function App() {
       <div className="container my-5">
         <div className="col">
           <h2 className="text-center mb-5">Absenzformular</h2>
-          <Form action={handleSubmit}>
-            <FormGroup row>
-              <Label
-                  for="employeeName"
-                  sm={2}
-              >
-                Mitarbeiter:in
-              </Label>
-              <Col sm={10}>
-                <Input
-                    id="employeeName"
-                    name="employeeName"
-                    placeholder="Name eingeben"
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label
-                  for="selectAbsenceType"
-                  sm={2}
-              >
-                Abwesenheitstyp
-              </Label>
-              <Col sm={10}>
-                <Input
-                    id="selectAbsenceType"
-                    name="selectAbsenceType"
-                    type="select"
-                >
-                  <option value={0}>Urlaub</option>
-                  <option value={1}>Krankheit</option>
-                  <option value={2}>Training</option>
-                  <option value={3}>Anderes</option>
-                </Input>
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label
-                  for="startDate"
-                  sm={2}
-              >
-                Startdatum
-              </Label>
-              <Col sm={10}>
-                <Input
-                    id="startDate"
-                    name="startDate"
-                    placeholder="date placeholder"
-                    type="date"                     
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label
-                  for="endDate"
-                  sm={2}
-              >
-                Enddatum
-              </Label>
-              <Col sm={10}>
-                <Input
-                    id="endDate"
-                    name="endDate"
-                    placeholder="date placeholder"
-                    type="date"
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label
-                  for="comment"
-                  sm={2}
-              >
-                Kommentare
-              </Label>
-              <Col sm={10}>
-                <Input
-                    id="comment"
-                    name="comment"
-                    type="textarea"                     
-                />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Col sm={2}>
-                <Button primary>
-                  Neue Anfrage erstellen
-                </Button>
-              </Col>
-            </FormGroup>
-          </Form>
+            <RequestForm />
         </div>
       </div>
     </div>
---
+++ Absence-Management-API/client-app/src/components/RequestTableRows.js
@@ -0,0 +1,42 @@
+﻿import React from 'react';
+import {Button, ButtonGroup} from "reactstrap";
+
+const RequestTableRows = props =>
+    props.requests.map((request, index) => (
+        <tr>
+            <th scope="row">{request.id}</th>
+            <td>{request.employeeName}</td>
+            <td>{request.absenceType}</td>
+            <td>{request.startDate.slice(0, 10)}</td>
+            <td>{request.endDate.slice(0, 10)}</td>
+            <td>{request.comment}</td>
+            <td>{request.absenceStatus}</td>
+            <td>
+                <ButtonGroup>
+                    <Button
+                        data-id={request.id}
+                        color="success"
+                        // onClick={approveRequest}
+                    >
+                        Approve
+                    </Button>
+                    <Button
+                        data-id={request.id}
+                        color="warning"
+                        // onClick={denyRequest}
+                    >
+                        Deny
+                    </Button>
+                    <Button
+                        data-id={request.id}
+                        color="danger"
+                        // onClick={deleteRequest}
+                    >
+                        Delete
+                    </Button>
+                </ButtonGroup>
+            </td>
+        </tr>
+    ));
+
+export default RequestTableRows;
