14de9bfd7fe9514a133a4ee876f73d5d12012ed6
Vic Golding <19528516+vicgolding@users.noreply.github.com>
2026-07-14 17:21:28 +0200

    Configure Chai test

---
+++ Absence-Management-API/client-app/src/textUtilities.js
@@ -0,0 +1,14 @@
+﻿import { expect } from 'chai';
+
+function nameCase(fullName) {
+    const names = fullName.split(' ');
+    const titleCasedName = names.map((name) => {
+        return name[0].toUpperCase() + name.substring(1);
+    });
+    return titleCasedName.join(' ');
+}
+
+expect(nameCase('arthur morgan')).to.be.a('string');
+expect(nameCase('a')).to.equal('A');
+expect(nameCase('arthur')).to.equal('Arthur');
+expect(nameCase('arthur morgan')).to.equal('Arthur Morgan');
