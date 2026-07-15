f73df0fc74c430a4dcd80cd957c6d7dbf90ce545
Vic Golding <19528516+vicgolding@users.noreply.github.com>
2026-07-15 15:49:03 +0200

    Refactor arrow functions

--- Absence-Management-API/client-app/test/functions.test.js
+++ Absence-Management-API/client-app/test/functions.test.js
@@ -5,40 +5,40 @@ import absenceTypes from '../src/data/absenceTypes.json' with { type: 'json' };
 import absenceStatuses from '../src/data/absenceStatuses.json' with { type: 'json' };
 
 // describe() introduces a test suite - a block of similar unit tests or "test specs"
-describe("Mocha", function () {
+describe("Mocha", () => {
     // it() introduces a single unit test - sometimes called a "spec", formed a group of similar expectations
-   it("should be running when I run 'npm test'", function () {
+   it("should be running when I run 'npm test'", () => {
        // a sanity check is a trivial function or test that proves we set things up correctly
        expect(true).to.be.ok;
    }); 
 });
 
-describe("formatName", function () {
+describe("formatName", () => {
     // TODO: function should only accept string type
     let fullName; 
     let expected;
-    before(function () {
+    before(() => {
         fullName = "arthur morgan";
         expected = "Arthur Morgan";
     })
     
-    it("should correctly convert a full name to uppercase", function () {
+    it("should correctly convert a full name to uppercase", () => {
         equal(formatName(fullName), expected);
     });
 });
 
-describe("absenceTypes", function () {
+describe("absenceTypes", () => {
     const data = JSON.stringify(absenceTypes);
     const types = data.split(',');
-   it("should have a length of 4", function () {
+   it("should have a length of 4", () => {
        assert.lengthOf(types, 4);
    });
 });
 
-describe("absenceStatuses", function () {
+describe("absenceStatuses", () => {
     const data = JSON.stringify(absenceStatuses);
     const statuses = data.split(',');
-    it("should have a length of 3", function () {
+    it("should have a length of 3", () => {
         assert.lengthOf(statuses, 3);
     });
 });
