60cb66509eae609079ee74a68604e92e64dcae74
Vic Golding <19528516+vicgolding@users.noreply.github.com>
2026-07-16 12:04:51 +0200

    Test first Selenium script

---
+++ Absence-Management-API/client-app/test/firstScript.spec.js
@@ -0,0 +1,24 @@
+﻿import {By, Builder, Browser} from 'selenium-webdriver';
+
+let driver;
+try {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('http://localhost:3000/');
+    await driver.manage().setTimeouts({implicit: 5000});
+    let employeeNameField = await driver.findElement(By.name("employeeName"));
+    let submitButton = await driver.findElement(By.id("submit-button"));
+    let startDateField = await driver.findElement(By.name("startDate"));
+    let endDateField = await driver.findElement(By.name("endDate"));
+    await employeeNameField.sendKeys("Arthur Morgan");
+    await driver.manage().setTimeouts({implicit: 5000});
+    await startDateField.sendKeys("01-01-2026");
+    await driver.manage().setTimeouts({implicit: 5000});
+    await endDateField.sendKeys("02-01-2026");
+    await driver.manage().setTimeouts({implicit: 5000});
+    await submitButton.click();
+    await driver.manage().setTimeouts({implicit: 7500});
+} catch (error) {
+    console.error(error);
+} finally {
+    await driver.quit();
+}
